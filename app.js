7fcc792551d9d70a30b67c596487260fa65e5b9f
kimnamjoon123 <poojasaini152018@gmail.com>
2022-02-24 21:15:07 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,170 @@
+
+let questions = [
+
+  {
+
+    numb: 1,
+
+
+
+    question: "Kuala Lumpur is the capital of",
+
+
+
+    answer: "B Malaysia",
+
+
+
+    options: ["A Vietnam", "B Malaysia", "C Sweden", "D Austria"],
+
+
+
+    src: "",
+
+  },
+
+
+
+  {
+
+    numb: 2,
+
+
+
+    question: "Which country does this flag belong to?",
+
+
+
+    answer: "B Finland",
+
+
+
+    options: ["A Vietnam", "B Finland", "C Sweden", "D Austria"],
+
+
+
+    src: "download.png",
+
+  },
+
+
+
+  {
+
+    numb: 3,
+
+
+
+    question: "What does PHP stand for?",
+
+
+
+    answer: "A Hypertext Preprocessor",
+
+
+
+    options: [
+
+      "A Hypertext Preprocessor",
+
+
+
+      "B Hypertext Programming",
+
+
+
+      "C Hypertext Preprogramming",
+
+
+
+      "D Hometext Preprocessor",
+
+    ],
+
+
+
+    src: "",
+
+  },
+
+
+
+  {
+
+    numb: 4,
+
+
+
+    question: "What does SQL stand for?",
+
+
+
+    answer: "D Structured Query Language",
+
+
+
+    options: [
+
+      "A Stylish Question Language",
+
+
+
+      "B Stylesheet Query Language",
+
+
+
+      "C Statement Question Language",
+
+
+
+      "D Structured Query Language",
+
+    ],
+
+
+
+    src: "",
+
+  },
+
+
+
+  {
+
+    numb: 5,
+
+
+
+    question: "What does XML stand for?",
+
+
+
+    answer: "A eXtensible Markup Language",
+
+
+
+    options: [
+
+      "A eXtensible Markup Language",
+
+
+
+      "B eXecutable Multiple Language",
+
+
+
+      "C eXTra Multi-Program Language",
+
+
+
+      "D eXamine Multiple Language",
+
+    ],
+
+
+
+    src: "",
+
+  },
+
+];
